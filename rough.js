7d518047b310b18bb6c780b497ccb4bc4eca2887
Kalluri Abhinandan Kumar <abhinandankalluri@gmail.com>
2023-03-16 15:36:54 +0530

    rough

--- rough.js
+++ rough.js
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 l1 = { 1: 1, 2: 2, 3: 3 }
 
 l2 = { 1: 1, 2: 2, 3: 3 }
@@ -6,3 +7,32 @@ l2[1] = 1000
 
 console.log(l1)
 console.log(l2)
+=======
+// const https = require('https');
+
+// const projectId = '2MglFT530Bq91IHREYXBpWUaIFz';
+// const projectSecret = '361fd97a85175946dd71e3e6c8f883c3';
+
+// const options = {
+//     host: 'ipfs.infura.io',
+//     port: 5001,
+//     path: '/api/v0/get?arg=QmYQPUSSi2YZr8V8j9ZUYVerq1bECmvoKXX7rLVRxYadHu',
+//     method: 'POST',
+//     auth: projectId + ':' + projectSecret,
+// };
+
+// let req = https.request(options, (res) => {
+//     let body = '';
+//     res.on('data', function (chunk) {
+//         body += chunk;
+//     });
+//     res.on('end', function () {
+//         console.log(body);
+//     });
+// });
+// req.end();
+
+li = [1 , 2 ,3 ]
+
+for(l from)
+>>>>>>> Stashed changes
